b5808f507d75ee10d55ea4e0cec26353733aa142
Bartlomiej-Wieloch <bartlomiej.wieloch@microsoft.wsei.edu.pl>
2025-03-09 16:02:32 +0100

    Completed 'Krok 1'

---
+++ Bank/Bank/Konto.cs
@@ -0,0 +1,51 @@
+﻿#nullable disable
+using System.Numerics;
+
+namespace Bank
+{
+    public class Konto
+    {
+        private string klient;
+        private decimal bilans;
+        private bool zablokowane = false;
+
+        public string Nazwa { get; } 
+        public decimal Bilans { get; private set; } //Wbrew 7. punktu z kroku 1, musze dać tutaj private set aby testy przeszedły, 
+        public bool Blokada { get; private set; }   //gdy jest tylko get (do odczytu) nie mogę zmodyfikować wartości za pomocą metod
+
+        public Konto (string Klient, decimal bilansNaStart = 0)
+        {
+            klient = Klient;
+            bilans = bilansNaStart;
+            Nazwa = klient;
+            Bilans = bilans;
+            Blokada = zablokowane;
+        }
+
+        public void Wplata(decimal kwota)
+        {
+            if (Blokada)
+                throw new ArgumentException("Nie można dokonać wpłaty do zablokowanego konta");
+            if (kwota < 0)
+                throw new ArgumentException("Nie można dokonać ujemnej wpłaty");
+
+            Bilans += kwota;
+
+        }
+        public void Wyplata(decimal kwota)
+        {
+            if (Blokada)
+                throw new ArgumentException("Nie można dokonać wypłaty z zablokowanego konta");
+            if (kwota < 0)
+                throw new ArgumentException("Nie można dokonać ujemnej wypłaty");
+            if (Bilans < kwota)
+                throw new ArgumentException("Nie można wypłacić więcej środków niż jest na koncie");
+
+            Bilans -= kwota;
+        }
+
+        public void BlokujKonto() => Blokada = true;
+        public void OdblokujKonto() => Blokada = false;
+    }
+
+}
---
+++ Bank/BankTests/BankTests.cs
@@ -0,0 +1,152 @@
+﻿using Bank;
+using Newtonsoft.Json.Converters;
+
+
+namespace BankTests
+{
+    [TestClass]
+    public sealed class BankTests
+    {
+        [TestMethod]
+        public void Konstruktor_PoprawneDane()
+        {
+            //Arange
+            string nazwa = "Bartek";
+            decimal saldo = 500;
+
+            //Act
+            var km = new Konto(nazwa, saldo);
+
+            //Assert
+            Assert.AreEqual(nazwa, km.Nazwa);
+            Assert.AreEqual(saldo, km.Bilans);
+            Assert.AreEqual(false, km.Blokada); 
+        }
+        [TestMethod]
+        public void BlokujKonto_KontoOdblokowane_Powodzenie()
+        {
+            //Arange
+            string nazwa = "Bartek";
+            decimal saldo = 500;
+            var km = new Konto(nazwa, saldo);
+
+            //Act
+            km.BlokujKonto();
+
+            //Assert
+            Assert.IsTrue(km.Blokada);
+        }
+        [TestMethod]
+        public void OdblokujKonto_KontoZablokowane_Powodzenie()
+        {
+            //Arange
+            string nazwa = "Bartek";
+            decimal saldo = 500;
+            var km = new Konto(nazwa, saldo);
+
+            //Act
+            km.OdblokujKonto();
+
+            //Assert
+            Assert.IsFalse(km.Blokada);
+        }
+        [TestMethod]
+        public void Wplata_KwotaDodatnia_SaldoZwiekszaSie()
+        {
+            //Arange
+            string nazwa = "Bartek";
+            decimal saldo = 500;
+            var km = new Konto(nazwa, saldo);
+
+            //Act
+            km.Wplata(500);
+
+            //Assert
+            Assert.AreEqual(1000, km.Bilans);
+        }
+        [TestMethod]
+        public void Wplata_KwotaUjemna_Exception()
+        {
+            //Arange
+            string nazwa = "Bartek";
+            decimal saldo = 500;
+            var km = new Konto(nazwa, saldo);
+
+            //Act
+
+            //Assert
+            Assert.ThrowsException<ArgumentException>(() => km.Wplata(-500));
+        }
+        [TestMethod]
+        public void Wplata_KontoZablokowane_Exception()
+        {
+            //Arange
+            string nazwa = "Bartek";
+            decimal saldo = 500;
+            var km = new Konto(nazwa, saldo);
+            km.BlokujKonto();
+
+            //Act
+
+            //Assert
+            Assert.ThrowsException<ArgumentException>(() => km.Wplata(500));
+        }
+        [TestMethod]
+        public void Wyplata_KwotaDodatnia_SaldoZmniejszaSie()
+        {
+            //Arange
+            string nazwa = "Bartek";
+            decimal saldo = 500;
+            var km = new Konto(nazwa, saldo);
+
+            //Act
+            km.Wyplata(500);
+
+            //Assert
+            Assert.AreEqual(0, km.Bilans);
+        }
+        [TestMethod]
+        public void Wyplata_KwotaUjemna_Exception()
+        {
+            //Arange
+            string nazwa = "Bartek";
+            decimal saldo = 500;
+            var km = new Konto(nazwa, saldo);
+
+            //Act
+
+            //Assert
+            Assert.ThrowsException<ArgumentException>(() => km.Wyplata(-500));
+        }
+        [TestMethod]
+        public void Wyplata_BrakSrodkow_Exception()
+        {
+            //Arange
+            string nazwa = "Bartek";
+            decimal saldo = 500;
+            var km = new Konto(nazwa, saldo);
+
+            //Act
+
+            //Assert
+            Assert.ThrowsException<ArgumentException>(() => km.Wyplata(600));
+
+        }
+        [TestMethod]
+        public void Wyplata_KontoZablokowane_Exception()
+        {
+            //Arange
+            string nazwa = "Bartek";
+            decimal saldo = 500;
+            var km = new Konto(nazwa, saldo);
+            km.BlokujKonto();
+
+            //Act
+
+            //Assert
+            Assert.ThrowsException<ArgumentException>(() => km.Wyplata(500));
+
+        }
+
+    }
+}
