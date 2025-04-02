45aa2f06d97c7b8535f5af7f03aabe046f88f56b
Bartlomiej-Wieloch <bartlomiej.wieloch@microsoft.wsei.edu.pl>
2025-04-02 11:52:44 +0200

    Fix delegation for step 3

--- Bank/Bank/KontoLimit.cs
+++ Bank/Bank/KontoLimit.cs
@@ -9,17 +9,21 @@ namespace Bank
 {
     public class KontoLimit
     {
-        private readonly KontoPlus _konto;
-        public decimal Limit { get; private set; }
+        private readonly Konto _konto;
+        private decimal _limitDebetowy;
 
         public KontoLimit(string Klient, decimal limitDebetowy, decimal bilansNaStart = 0)
         {
-            _konto = new KontoPlus(Klient, limitDebetowy, bilansNaStart);
-            Limit = limitDebetowy;
+            _konto = new Konto(Klient, bilansNaStart);
+            _limitDebetowy = limitDebetowy;
         }
 
+        public decimal Limit
+        {
+            get { return _limitDebetowy; }
+        }
         public string Nazwa
-        { 
+        {
             get { return _konto.Nazwa; }
         }
         public decimal Bilans
@@ -33,11 +37,36 @@ namespace Bank
 
         public void Wyplata(decimal kwota)
         {
-            _konto.Wyplata(kwota);
+            if (kwota < 0)
+                throw new ArgumentException("Nie można dokonać ujemnej wypłaty.");
+            if (_konto.Blokada)
+                throw new ArgumentException("Nie można dokonać wypłaty z zablokowanego konta.");
+            if (_konto.Bilans + _limitDebetowy < kwota) 
+                throw new ArgumentException("Nie można wypłacić kwoty większej niż dostępne środki + limit debetowy.");
+
+            _konto.Bilans -= kwota;
+
+            if (_konto.Bilans < 0)
+                BlokujKonto();
         }
         public void Wplata(decimal kwota)
         {
-            _konto.Wplata(kwota);
+            if (kwota < 0)
+                throw new ArgumentException("Nie można dokonać ujemnej wpłaty.");
+
+            bool byloZablokowane = _konto.Blokada;
+
+            if (byloZablokowane)
+            {
+                _konto.Bilans += kwota;
+            }
+            else
+            {
+                _konto.Wplata(kwota);
+            }
+
+            if (byloZablokowane && _konto.Bilans > 0)
+                OdblokujKonto();
         }
 
         public void BlokujKonto()
@@ -51,15 +80,21 @@ namespace Bank
 
         public void ZwiekszLimit(decimal wartosc)
         {
-            decimal aktualnyLimit = _konto.Limit;
-            _konto.ZwiekszLimit(wartosc);
-            Limit = aktualnyLimit + wartosc;
+            if (wartosc < 0)
+                throw new ArgumentException("Nie można zwiększyć limitu o ujemną wartość.");
+            _limitDebetowy += wartosc;
         }
         public void ZmniejszLimit(decimal wartosc)
         {
-            decimal aktualnyLimit = _konto.Limit; 
-            _konto.ZmniejszLimit(wartosc);
-            Limit = aktualnyLimit - wartosc; 
+            if (wartosc < 0)
+                throw new ArgumentException("Nie można przetworzyć ujemnej wartości");
+
+            if (_limitDebetowy - wartosc < 0)
+                throw new ArgumentException("Nie można zmniejszyć limitu debetowego do wartosci ujemnej");
+
+
+
+            _limitDebetowy -= wartosc;
         }
     }
 }
